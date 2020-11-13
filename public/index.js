a5bcb9c89f5ea486ddf5edc06f2a0a42a0046612
rafapuerta <info@rafapuerta.com>
2020-11-13 19:23:47 +0100

    first commit

---
+++ public/index.js
@@ -0,0 +1,7 @@
+function newGame(){
+    /* llamada al back para recibir los links de las imágenes a entregar */
+}
+
+function top25(){
+    /* llama al back para que devuelva el top25 de las mejores puntuaciones. Luego las imprime en el ID designado */
+}
