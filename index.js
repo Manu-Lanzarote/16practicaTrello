cd4cda081c6597419a622158e0d7a95aa1ac76be
Manu GB <Manu@MacBook-Air-de-manu.local>
2020-11-13 19:16:14 +0000

    second commit

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//App.get que llamará a la api de google
+
+app.get("/nuevaPartida", function (req, res) {
+  fetch(
+    "https://www.googleapis.com/customsearch/v1?key=AIzaSyCZfnGYC_O2gVusW20V7sSmke3f2ckh04Y&cx=017576662512468239146:omuauf_lfve&q=lectures"
+  )
+    .then(function (respuesta) {
+      return respuesta.json();
+    })
+    .then(function (datos) {
+      res.send(datos);
+    });
+});
+
 app.listen(3000);
